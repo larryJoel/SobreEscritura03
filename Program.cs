28db10fee6bd0f916daca962f19e6dba77f7c89e
Larry González <larry.g.m1024@gmail.com>
2022-10-08 17:18:21 -0300

    my first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program{
+    static void Main(string[] args){
+        Program prog = new Program();
+        var sumaEnteros =  prog.Sumar(3,8);
+        var sumaTexto = prog.Sumar("Hola"," Mundo");
+        var sumaReales = prog.Sumar(2.15,15.254);
+        Console.WriteLine("Sumando enteros: "+sumaEnteros);
+        Console.WriteLine("Suma reales: "+ sumaReales);
+        Console.WriteLine("Suma texto: "+sumaTexto);
+    }
+
+    int Sumar(int num1, int num2){
+        int suma;
+        suma = num1+num2;
+        return suma;
+    }
+    string Sumar(string a, string b){
+        return a+b;
+    }
+    double Sumar(double num1, double num2){
+        double suma;
+        suma = num1+num2;
+        return suma;
+    }
+}
